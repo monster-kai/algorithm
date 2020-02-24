978e79ddd2c996158fb0a936514c5edb3a90ae6f
Ryan <919381328@qq.com>
2020-02-24 22:11:33 +0800

    栈

--- code/src/stack/_Stack.java
+++ code/src/stack/_Stack.java
@@ -19,6 +19,10 @@ public class _Stack {
         reverse(s);
         s.push(e);
     }
+
+    /**
+     *取出栈底元素
+     */
     private static int removeLast(Stack<Integer> s){
         int element = s.pop();
         if(s.isEmpty()){
@@ -29,13 +33,29 @@ public class _Stack {
             return last;
         }
     }
+    //利用一个辅助栈给一个栈元素排序 顶为最大
+    //思路： 取出元素放入辅助栈中，使得辅助栈的元素一直保持有序， 每次迭代都将curr放到正确的位置
+    public static void sortInStack(Stack<Integer> s){
+        Stack<Integer> aux = new Stack<>();
+        while(!s.isEmpty()){
+            int curr = s.pop();
+            if(!aux.isEmpty() && curr>aux.peek()){
+                int w = aux.peek();
+                s.push(aux.pop());
+            }
+            aux.push(curr);
+        }
+        while(!aux.isEmpty())  s.push(aux.pop());
+    }
 
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>(){{
-            push(1); push(2); push(3);
+            push(3); push(2); push(7); push(12); push(5);
         }};
         System.out.println(s);
         reverse(s);
+        System.out.println(s);
+        sortInStack(s);
         System.out.println(s);
     }
 }
